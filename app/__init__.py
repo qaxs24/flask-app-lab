5d0819d852896c457aee9601f9cbe0b76dfb040c
Іван Олійник <ivan.oliinyk.23@pnu.edu.ua>
2025-10-20 19:08:38 +0300

    feat: Add flash message support in base template

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a_very_secret_key_that_should_be_changed'
 
 from app.portfolio import portfolio_bp
 app.register_blueprint(portfolio_bp)
 
 from app.products import products_bp
 app.register_blueprint(products_bp)
+
+from app.users import users_bp
+app.register_blueprint(users_bp)
